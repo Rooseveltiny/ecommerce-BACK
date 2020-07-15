ebaffbe73c727589b2ea7345ff9a92f1a298d8e4
Rooseveltiny <45924428+Rooseveltiny@users.noreply.github.com>
2020-07-15 16:05:14 +0300

    added details
    details

--- ecommerce/shop/serializers.py
+++ ecommerce/shop/serializers.py
@@ -4,16 +4,21 @@ from shop.models import Detail, DetailGroup, Product
 class ProductsListSerializer(serializers.ModelSerializer):
 
     detail = serializers.StringRelatedField(many=True)
-
     class Meta: 
         model = Product
         fields = ('link', 'title', 'unit_of_measurement', 'price', 'balance', 'detail')
 
+class DetailsSerializer(serializers.ModelSerializer):
+
+    detail_group = serializers.StringRelatedField()
+
+    class Meta:
+        model = Detail
+        fields = ('title', 'link', 'detail_group')
+
 class ProductSerializer(serializers.ModelSerializer):
 
-    detail = serializers.StringRelatedField(many=True)
-    
+    detail = DetailsSerializer(many=True)    
     class Meta:
-
         model = Product
         fields = '__all__'
